cf1e25b185b560b4942de602e60f75dec2b370a2
Laura Macchi <lau.macchi@gmail.com>
2017-04-10 22:44:06 -0500

    Add stats and bind to 0.0.0.0

--- site/profile/manifests/proxy.pp
+++ site/profile/manifests/proxy.pp
@@ -1,8 +1,15 @@
 class profile::proxy {
-  include ::haproxy
+  class { 'haproxy':
+    defaults_options => {
+      'log'     => 'global',
+      'stats'   => 'enable',
+      'maxconn' => '8000',
+    },
+  }
+  
   haproxy::listen { 'puppet00':
     mode      => 'tcp',
-    ipaddress => $::ipaddress,
+    ipaddress => '0.0.0.0',
     ports     => '8140',
     options   => {
       'option'  => [
