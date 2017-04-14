a5d131905d0d999ca8a6db385e5ba09cdd4d46f2
Laura Macchi <lau.macchi@gmail.com>
2017-04-14 13:29:14 -0500

    Add supporting plugins

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -3,5 +3,12 @@ class profile::jenkins {
 
   jenkins::plugin { 'puppet-enterprise-pipeline': }
   jenkins::plugin { 'plain-credentials': }
+  jenkins::plugin { 'workflow-api': }
+  jenkins::plugin { 'workflow-basic-steps': }
+  jenkins::plugin { 'workflow-cps': }
+  jenkins::plugin { 'workflow-durable-task-step': }
+  jenkins::plugin { 'credentials': }
+  jenkins::plugin { 'script-security': }
+  jenkins::plugin { 'structs': }
   
 }
