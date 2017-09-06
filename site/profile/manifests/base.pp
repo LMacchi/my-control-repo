09eb3c4e19401475314bb01f5ee4f2cedf8cd729
Laura Macchi <lm@puppet.com>
2017-09-06 23:31:33 +0000

    Remove trailing whitespaces

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,7 @@
 # @summary This profile configures /etc/hosts
 class profile::base {
 
-  
+
   if $trusted['extensions']['pp_role'] {
     $line  = "PS1='\$USER@${trusted['extensions']['pp_role']} \$PWD> '"
     $alias = $trusted['extensions']['pp_role']
@@ -9,28 +9,28 @@ class profile::base {
     $line = "PS1='\$USER@${facts['fqdn']} \$PWD> '"
     $alias = $facts['hostname']
   }
-  
-  
+
+
   @@host { $facts['fqdn']:
     ensure       => present,
     host_aliases => $alias,
     ip           => $facts['ipaddress'],
     tag          => 'puppet',
   }
-  
+
   Host <<| tag == 'puppet' |>>
-  
+
   # Ensure Vagrant/CentOS users have sudo access
   sudo::conf { 'Wheel':
     ensure  => 'present',
     content => '%wheel    ALL=(ALL)       NOPASSWD: ALL',
   }
-  
+
   include vim
   vim::vim_profile { 'root': }
-  
+
   include git
-  
+
   git::config { 'user.name':
     value => 'Laura Macchi',
   }
