8cecd1f868ae0687ad3f83b80689dd8f81f8c7af
LMacchi <lau.macchi@gmail.com>
2017-10-30 13:10:43 -0500

    Add classification code

--- site/role/manifests/puppet/cm.pp
+++ site/role/manifests/puppet/cm.pp
@@ -1,5 +1,6 @@
 # @summary Role that provisions a PE Compile Master
 class role::puppet::cm {
   include profile::base
+  include profile::puppet::master
   include profile::puppet::compile_master
 }
--- site/role/manifests/puppet/master.pp
+++ site/role/manifests/puppet/master.pp
@@ -1,6 +1,7 @@
-# @summary Role to provision a PE Master
+# @summary Role to provision a PE MoM
 class role::puppet::master {
   include profile::base
   include profile::puppet::master
+  include profile::puppet::classifier
   include profile::vra_config
 }
