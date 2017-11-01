f6f35342ba33fa1c17c5d643ed200c00d117db69
Laura Macchi <lau.macchi@gmail.com>
2017-11-01 17:02:59 -0500

    Disable security

--- site/profile/manifests/jenkins2.pp
+++ site/profile/manifests/jenkins2.pp
@@ -8,28 +8,38 @@ class profile::jenkins2 {
   }
   
   rpmkey { 'D50582E6':
-    ensure => 'present',
-    source => 'https://jenkins-ci.org/redhat/jenkins-ci.org.key',
+    ensure  => present,
+    source  => 'https://jenkins-ci.org/redhat/jenkins-ci.org.key',
+    require => Yumrepo['jenkins'],
   }
   
-  package { ['java','jenkins']:
+  package { 'java':
     ensure => present,
+  }
+  
+  package { 'jenkins':
+    ensure  => present,
+    require => [Package['java'],Yumrepo['jenkins'],Rpmkey['D50582E6']],
+  }
+  
+  # Disable security
+  file_line { 'disable security in jenkins':
+    path    => '/var/lib/jenkins/config.xml',
+    line    => '<useSecurity>false</useSecurity>',
+    match   => '<useSecurity>.*<\/useSecurity>',
+    require => Package['jenkins'],
+    notify  => Service['jenkins'],
   }
   
   # Disable setup wizard
   file { '/etc/default/jenkins':
     ensure  => file,
     content => 'JAVA_ARGS=Djenkins.install.runSetupWizard=false',
+    require => Package['jenkins'],
+    notify  => Service['jenkins'],
   }
   
   service { 'jenkins':
     ensure => running,
   }
-  
-  Yumrepo['jenkins'] 
-  -> Rpmkey['D50582E6']
-  -> Package['java']
-  -> Package['jenkins']
-  -> File['/etc/default/jenkins']
-  ~> Service['jenkins']
 }
