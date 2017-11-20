7dd1e3b0c85e691c85f20a43bf06556d8952aa71
Laura Macchi <lau.macchi@gmail.com>
2017-11-20 17:31:52 -0600

    Fix little error

--- site/profile/lib/facter/networkzone.rb
+++ site/profile/lib/facter/networkzone.rb
@@ -2,7 +2,7 @@ require "ipaddr"
 Facter.add(:networkzone) do
   setcode do
     result = 'dmz'
-    network = Facter.value(:networking)['interfaces'].each do |iface, values|
+    Facter.value(:networking)['interfaces'].each do |iface, values|
       if IPAddr.new("172.16.0.0/16").include?(values['ip'])
         result = 'internal'
         break # I found the internal ip, don't bother continuing
