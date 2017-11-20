350009767de8d42c4a9f130ea58b8ecc74d39c13
Laura Macchi <lau.macchi@gmail.com>
2017-11-20 17:27:20 -0600

    Add silent flag to curl

--- site/profile/lib/facter/external_ip.rb
+++ site/profile/lib/facter/external_ip.rb
@@ -1,5 +1,5 @@
 Facter.add(:external_ip) do
   setcode do
-    Facter::Core::Execution.execute('curl http://169.254.169.254/latest/meta-data/public-ipv4', options = {:timeout => 2}).chomp
+    Facter::Core::Execution.execute('curl -s http://169.254.169.254/latest/meta-data/public-ipv4', options = {:timeout => 2}).chomp
   end
 end
