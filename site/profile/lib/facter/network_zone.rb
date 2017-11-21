2e7a8205d955a779d1e7be7148716011ff5c353c
Laura Macchi <lau.macchi@gmail.com>
2017-11-21 16:05:54 -0600

    Try confine

--- site/profile/lib/facter/network_zone.rb
+++ site/profile/lib/facter/network_zone.rb
@@ -1,15 +1,16 @@
 require 'ipaddr'
 Facter.add(:network_zone) do
+  confine do
+    !Facter.value(:networking)['interfaces'].nil?
+  end
   setcode do
-    net = Facter.value(:networking)['interfaces'].nil? ? 'none' : 'dmz'
-    unless net.match('none')
-      Facter.value(:networking)['interfaces'].each do |iface, values|
-        if IPAddr.new("172.16.0.0/16").include?(values['ip'])
-          net = 'internal'
-          break # I found the internal ip, don't bother continuing
-        else
-          next
-        end
+    net = 'dmz'
+    Facter.value(:networking)['interfaces'].each do |iface, values|
+      if IPAddr.new("172.16.0.0/16").include?(values['ip'])
+        net = 'internal'
+        break # I found the internal ip, don't bother continuing
+      else
+        next
       end
     end
   net
