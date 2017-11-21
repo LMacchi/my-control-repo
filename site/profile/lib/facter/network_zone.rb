1889c315402c1c8b12590e9a016bc97bd88314ea
Laura Macchi <lau.macchi@gmail.com>
2017-11-21 15:50:00 -0600

    Add condition for empty networking

--- site/profile/lib/facter/network_zone.rb
+++ site/profile/lib/facter/network_zone.rb
@@ -1,17 +1,17 @@
 require 'ipaddr'
 Facter.add(:network_zone) do
   setcode do
-    # Exit with 'none' if there is no networking fact
-    return 'none' unless !Facter.value(:networking)['interfaces'].nil?
-    result = 'dmz'
-    Facter.value(:networking)['interfaces'].each do |iface, values|
-      if IPAddr.new("172.16.0.0/16").include?(values['ip'])
-        result = 'internal'
-        break # I found the internal ip, don't bother continuing
-      else
-        next
+    net = Facter.value(:networking)['interfaces'].nil? ? 'none' : 'dmz'
+    unless net.match('none')
+      Facter.value(:networking)['interfaces'].each do |iface, values|
+        if IPAddr.new("172.16.0.0/16").include?(values['ip'])
+          net = 'internal'
+          break # I found the internal ip, don't bother continuing
+        else
+          next
+        end
       end
     end
-  result
+  net
   end
 end
