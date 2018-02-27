4a56f74dae5b9b8af28d840442a5744e4fa4ee67
Laura Macchi <lau.macchi@gmail.com>
2018-02-27 13:56:36 -0600

    Add global var role

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,6 +25,10 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+$role = $trusted['extensions']['pp_role'] ? {
+  undef   => 'NONE',
+  default => $trusted['extensions']['pp_role']
+}
 
 node default {
   # This is where you can declare classes for all nodes.
