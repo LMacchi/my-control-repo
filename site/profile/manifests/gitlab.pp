1020db49cf95f973c26ed6a773d43951825260f1
Laura Macchi <lau.macchi@gmail.com>
2018-06-28 17:47:21 -0500

    Update gitlab url

--- site/profile/manifests/gitlab.pp
+++ site/profile/manifests/gitlab.pp
@@ -19,16 +19,16 @@ class profile::gitlab {
   }
 
   exec { 'create gitlab crt':
-    command => 'openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Texas/L=Dallas/O=Puppet/CN=gitlab.puppetlabs.vm" -keyout "/etc/gitlab/ssl/gitlab.puppetlabs.vm.key" -out "/etc/gitlab/ssl/gitlab.puppetlabs.vm.crt"',
-    creates => '/etc/gitlab/ssl/gitlab.puppetlabs.vm.crt',
+    command => 'openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Texas/L=Dallas/O=Puppet/CN=gitlab.lmacchi.vm" -keyout "/etc/gitlab/ssl/gitlab.lmacchi.vm.key" -out "/etc/gitlab/ssl/gitlab.lmacchi.vm.crt"',
+    creates => '/etc/gitlab/ssl/gitlab.lmacchi.vm.crt',
     require => File['/etc/gitlab/ssl'],
   }
 
   class { '::gitlab':
     external_url => 'https://gitlab.puppetlabs.vm',
     nginx        => {
-      ssl_certificate     => '/etc/gitlab/ssl/gitlab.puppetlabs.vm.crt',
-      ssl_certificate_key => '/etc/gitlab/ssl/gitlab.puppetlabs.vm.key',
+      ssl_certificate     => '/etc/gitlab/ssl/gitlab.lmacchi.vm.crt',
+      ssl_certificate_key => '/etc/gitlab/ssl/gitlab.lmacchi.vm.key',
     },
     require      => Exec['create gitlab crt'],
   }
